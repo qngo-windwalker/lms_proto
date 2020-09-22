47ebed8087df8666bffa511826b68d5c1be71ce4
Quan-home pc <quancreative@gmail.com>
2020-09-22 18:54:41 -0400

    Added English and Spanish verion of State of Illinois Sexual Harassment Pervention training

---
+++ web/modules/custom/wind_lms/course/gobierno-del-estado-de-illinois-curso-de-capacitacion-para-la-prevencion-del-acoso-sexual/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://Harassment Prevention Spanish"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Gobierno del Estado de Illinois: Curso de Capacitación para la Prevención del Acoso Sexual"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;El acoso sexual est&amp;aacute; prohibido en Illinois. Aprenda lo que puede hacer para asegurarse que no ocurrir&amp;aacute; en su lugar de empleo.&lt;/p&gt;"
+};
---
+++ web/modules/custom/wind_lms/course/state-of-illinois-sexual-harassment-prevention-training/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://Harassment Prevention English"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "State of Illinois Sexual Harassment Prevention Training"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;Sexual harassment is prohibited in Illinois. Learn what you can do to ensure it does not occur in your place of work.&amp;nbsp;&lt;/p&gt;"
+};
