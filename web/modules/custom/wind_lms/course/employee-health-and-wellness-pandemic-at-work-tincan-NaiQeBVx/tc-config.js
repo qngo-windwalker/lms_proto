fceba6563b2aaef19995195f53359c9b68a0afe3
Quan-home pc <quancreative@gmail.com>
2020-11-18 01:47:59 -0500

    Added new pandemic course.

---
+++ web/modules/custom/wind_lms/course/employee-health-and-wellness-pandemic-at-work-tincan-NaiQeBVx/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://tOf74Nvp4rNeR3OC3Xf27pbV_XDH-7Ew_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Employee Health and Wellness: Pandemic at Work"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;Each day, in the US, thousands are contracting and dying from COVID-19. As an employee who performs essential functions, and must continue to work outside your home, you are on the frontlines of the pandemic.&amp;nbsp;&lt;/p&gt;&lt;p&gt;&amp;nbsp;&lt;/p&gt;&lt;p&gt;Because of your exposure to the general public, you must understand how to protect yourself, those around you, and implement practices to reduce the spread of the virus. This 30-minute web-based training will give you tools you can apply immediately to protect yourself and others. It will cover things such as effective handwashing, correct use of Personal Protective Equipment (also known as PPE), and how to distance yourself from others when in a public setting correctly.&lt;/p&gt;"
+};
